2579889794c20d7ba6ae6c6237e93ca7f0633fd1
Agustín Freda <agustinfreda2004@gmail.com>
2023-04-17 15:30:15 -0300

    Ejercicio 4

--- ejercicios/ej04.go
+++ ejercicios/ej04.go
@@ -2,6 +2,9 @@ package ejercicios
 
 // Escriba un método recursivo que tome una cadena y
 // devuelva como resultado la cadena invertida.
-func Invertir(cadena string) string {
-	panic("Not implemented")
+func Invertir(cadena string) string { // O(n)
+	if len(cadena) == 0{
+		return cadena
+	}
+	return Invertir(cadena[1:]) + string(cadena[0])
 }
