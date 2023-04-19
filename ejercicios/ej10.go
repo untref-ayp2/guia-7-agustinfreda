eb8aa7dba9f3b3b541f4b39abeab27e717272f3a
Agustín Freda <agustinfreda2004@gmail.com>
2023-04-19 11:15:34 -0300

    Ejercicio 10

--- ejercicios/ej10.go
+++ ejercicios/ej10.go
@@ -1,8 +1,18 @@
 package ejercicios
 
+import "fmt"
+
 // Escribir un método recursivo que dado un número
 // entero decimal devuelva su equivalente en
 // binario en forma de string
-func DecimalABinario(n int) string {
-	panic("Not implemented")
+func DecimalABinario(decimal int) string {
+	fmt.Println("num:", decimal, "resto:",decimal%2)
+	if decimal == 0 {
+		return "0"
+	}
+	if decimal == 1 {
+		return "1"
+	}
+	resultado := fmt.Sprintf("%s%d", DecimalABinario(decimal/2), decimal%2)
+	return resultado
 }
